14631f3029e99b8e8c5c5861523da2876edb565f
Andrej Cieraskou <9002amc@gmail.com>
2022-10-27 18:32:21 +0300

    First commit

---
+++ StepsAnalyzer/Controllers/StepsAnalyzerHandler.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+
+using StepsAnalyzer.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepsAnalyzer.Controllers
+{
+    public static class StepsAnalyzerHandler
+    {
+        private static int day = 0;
+
+        private static readonly string pathToFolder = @"..\TestData";
+
+        public static readonly List<List<Walker>> walkersByNames = new();
+
+        public static string GetWalkersByNames()
+        {
+            string[] allFiles = Directory.GetFiles(pathToFolder, "*.json");
+            Array.Sort(allFiles);
+
+            foreach (var filename in allFiles)
+            {
+                day++;
+
+                Console.WriteLine(filename);        //test output
+
+                string pathToJsonFile = pathToFolder + filename;
+                using StreamReader r = new(pathToJsonFile);
+                string json = r.ReadToEnd();
+
+                List<Walker>? walkers = JsonConvert.DeserializeObject<List<Walker>>(json);
+
+                if (walkers == null) return "One or more files aren't readible";
+
+                bool itContains = false;
+
+                foreach (var walker in walkers)
+                {
+                    walker.Day = day;
+                    foreach (var walkerByName in walkersByNames)
+                        if (walkerByName.Any(n => n.Name == walker.Name))
+                        {
+                            walkerByName.Add(walker);
+                            itContains = true;
+                            break;
+                        }
+
+                    if (!itContains)
+                        walkersByNames.Add(new List<Walker>() { walker });
+
+                }
+            }
+
+            return "Data read successfully";
+        }
+
+        public static int AverageAmountOfSteps(List<Walker> walkDays)
+        {
+            int amountOfSteps = 0;
+            foreach (var walkDay in walkDays)
+                amountOfSteps += walkDay.AmountOfSteps;
+
+            return amountOfSteps / day;
+        }
+
+        public static (int, int) BestWorstResults(List<Walker> walkDays)
+        {
+            var results = new List<int>();
+            foreach (Walker walkDay in walkDays)
+                results.Add(walkDay.AmountOfSteps);
+
+            int[] resultsArr = results.ToArray();
+            Array.Sort(resultsArr);
+
+            var bestResult = resultsArr[resultsArr.Count() - 1];
+            var worstResult = resultsArr[0];
+
+            return (bestResult, worstResult);
+        }
+    }
+}
---
+++ StepsAnalyzer/Models/Walker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepsAnalyzer.Models
+{
+    public class Walker
+    {
+        public int Day { get; set; }
+        public int Rank { get; set; }
+        public string? Name { get; set; }
+        public string? Status { get; set; }      
+        public int AmountOfSteps { get; set; }  
+    }
+}
